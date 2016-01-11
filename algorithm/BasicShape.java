f5d7199366d67947afb88d4f1e235b6f3fc03770
FelixBrn <felix.brinkmann@gmx.net>
2016-01-11 01:22:56 +0100

    .

--- algorithm/BasicShape.java
+++ algorithm/BasicShape.java
@@ -6,15 +6,13 @@ public class BasicShape {
 	public BasicShape(ArrayList<MatrixHandler<Integer>> vectors, MatrixHandler adjMatrix){
 
 		ArrayList<Integer> dimensions = new ArrayList<Integer>();
-
-		//How can this work if calcDim( ) is void?
 		this.dimensions = calcDim(vectors);
 		this.adjMatrix = adjMatrix.clone();
 
 	}
 	
 	/**
-	 * @return the dimensions of a shape given an index. (you dont say :O what is this index?)
+	 * @return the dimensions of a shape given an index.
 	 */
 	public double getDimensions(int index){
 
