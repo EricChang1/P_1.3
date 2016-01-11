50b7b79deeda6fc615400b03f8ea781da80b2a20
MaGa <guser1996.gu@gmail.com>
2016-01-11 13:04:59 +0100

    removed syntax errors from basic shape class

--- algorithm/BasicShape.java
+++ algorithm/BasicShape.java
@@ -1,12 +1,24 @@
+package algorithm;
+
 import java.util.ArrayList;
+import algorithm.Matrix.*;
 
-public class BasicShape {
+public class BasicShape 
+{
+	public final static int DIM = 3;
+	
+	@SuppressWarnings("serial")
+ 	public static class BadNumberOfRowsException extends IllegalArgumentException
+ 	{
+ 		public BadNumberOfRowsException() {super(); }
+ 		
+ 		public BadNumberOfRowsException (String message) { super (message); }
+ 	}
 
-
-	public BasicShape(ArrayList<MatrixHandler<Integer>> vectors, MatrixHandler adjMatrix){
+	public BasicShape(ArrayList<IntegerMatrix> vectors, IntegerMatrix adjMatrix){
 
 		ArrayList<Integer> dimensions = new ArrayList<Integer>();
-		this.dimensions = calcDim(vectors);
+		calcDim(vectors);
 		this.adjMatrix = adjMatrix.clone();
 
 	}
@@ -14,64 +26,70 @@ public class BasicShape {
 	/**
 	 * @return the dimensions of a shape given an index.
 	 */
-	public double getDimensions(int index){
+	public int getDimensions(int index){
 
 		return dimensions.get(index);
 	}
+	
 	/** Calculates the dimensions of a shape
-	** @param vectors Arraylist containing all the vectors
+	** @param vectors ArrayList containing all the vectors
 	*/
-	public void calcDim(ArrayList<MatrixHandler<Integer>> vectors){
+	public void calcDim(ArrayList<IntegerMatrix> vectors){
 		
-		if !(numberOfMH(vectors)) throw new EmptyStackException();
+		if (!numberOfMH(vectors)) 
+			throw new BadNumberOfRowsException ("Vectors given to construct basic shape have different dimensions");
 
 			for(int i=0; i<vectors.size(); i++){
 
-				int max = maximum(vectors.get(i),i);
-				int min = minimum(vectors.get(i),i);
+				int max = maximum(vectors,i);
+				int min = minimum(vectors,i);
 				dimensions.add(max-min);
 
 		}
 	}
 	/** compares that all the Matrix Handlers have the same number of rows
-	* @param vectors Arraylist containing all the vectors
+	* @param vectors ArrayList containing all the vectors
 	* @return false if one Matrix Handler doesn't have the same number of rows
 	*/
-	public boolean numberOfMH(ArrayList<MatrixHandler<Integer>> vectors){
+	public boolean numberOfMH(ArrayList<IntegerMatrix> vectors){
 
 		int numberOfRows=vectors.get(0).getRows();
-		for(int temp: vectors){
-			if(temp!=numberOfRows)
+		for (int cVec = 0; cVec < vectors.size(); ++cVec)
+		{
+			if (vectors.get(cVec).getRows() != numberOfRows)
 				return false;
 		}
+		return true;
 	}
 	/** calculates the maximum vector value
-	* @param vector Arraylist containing all the vectors
+	* @param vector ArrayList containing all the vectors
 	* @param index The index of the vector in the Matrix Handler
 	* @return the maximum value.
 	*/
-	public int maximum(ArrayList<MatrixHandler<Integer>> vectors, int index){
+	public int maximum(ArrayList<IntegerMatrix> vectors, int index){
 
-	int max = Integer.MIN_VALUE;
-    	for(int temp: vectors){
-       		if(vectors.get(temp).get(index) > max){
-          		  max = vectors.get(temp).get(index);
-       		}
-   		}
+		int max = Integer.MIN_VALUE;
+		for (int cVec = 0; cVec < vectors.size(); ++cVec)
+		{
+	   		if (vectors.get(cVec).getCell (index, 0) > max){
+	      		  max = vectors.get(cVec).getCell (index, 0);
+	   		}
+		}
     	return max;
 
 	}
 	/** calculates the minimum vector value
-	* @param vector Arraylist containing all the vectors
+	* @param vector ArrayList containing all the vectors
 	* @param index The index of the vector in the Matrix Handler
 	* @return the minimum value.
 	*/
-	public int minimum(ArrayList<MatrixHandler<Integer>> vectors, int index){
-
-	int min = Integer.MAX_VALUE;
-    	for(int temp: vectors){
-       		if(vectors.get(temp).get(index) < min){
-          		  min= vectors.get(temp).get(index);
+	public int minimum(ArrayList<IntegerMatrix> vectors, int index)
+	{
+		int min = Integer.MAX_VALUE;
+    	for (int cVec = 0; cVec < vectors.size(); ++cVec)
+    	{
+       		if (vectors.get(cVec).getCell (index, 0) < min){
+          		  min = vectors.get(cVec).getCell (index, 0);
        		}
    		}
     	return min;
@@ -81,13 +99,13 @@ public class BasicShape {
 	 * @param index Finds corner point in this position
 	 * @return All the points that are connected with this point
      */
-	public Arraylist<MatrixHandler<Integer>> lookUpConnections(int index){
-
-	Arraylist<MatrixHandler<Integer>> connections = new Arraylist<MatrixHandler<Integer>>();
-	for(int counter=0; counter<adjMatrix.getRows(); counter++){
-		if(adjMatrix.getCell(index,counter)!=0){
-				connections.add(vectors.get(counter));
-			}
+	public ArrayList<IntegerMatrix> lookUpConnections(int index)
+	{
+		ArrayList<IntegerMatrix> connections = new ArrayList<IntegerMatrix>();
+		for(int counter=0; counter<adjMatrix.getRows(); counter++){
+			if(adjMatrix.getCell(index,counter)!=0){
+					connections.add(vectors.get(counter));
+				}
 		}
 		return connections;
 	}
@@ -97,25 +115,26 @@ public class BasicShape {
 	 * @param angle2 Desired amount of rotation in vertical axis (in degrees)
 	 * @return rotation matrix
 	 */
-	public MatrixHandler rotationMatrix(double angle1, double angle2){
-		double radAngle1 = Math.toRadian (angle1);
-		double radAngle2 = Math.toRadian (angle2);
+	public Matrix rotationMatrix(double angle1, double angle2){
+		double radAngle1 = Math.toRadians (angle1);
+		double radAngle2 = Math.toRadians (angle2);
 
-		MatrixHandler <Float> rotationMatrix1 = new MatrixHandler <Float> (3, 3);
+		Matrix <Double> rotationMatrix1 = new DoubleMatrix (DIM, DIM);
 		rotationMatrix1.setCell (0, 0, Math.cos (radAngle1));
 		rotationMatrix1.setCell (1, 0, Math.sin (radAngle1));
 		rotationMatrix1.setCell (0, 1, -Math.sin (radAngle1));
 		rotationMatrix1.setCell (1, 1, -Math.cos (radAngle1));
-		rotationMatrix1.setCell (2, 2, 1);
+		rotationMatrix1.setCell (2, 2, 1.0);
 
-		MatrixHandler <Float> rotationMatrix2 = new MatrixHandler <Float> (3, 3);
+		Matrix <Double> rotationMatrix2 = new DoubleMatrix (DIM, DIM);
 		rotationMatrix2.setCell (0, 0, Math.cos (radAngle2));
 		rotationMatrix2.setCell (1, 0, -Math.sin (radAngle2));
-		rotationMatrix2.setCell (0, 1, 1);
+		rotationMatrix2.setCell (0, 1, 1.0);
 		rotationMatrix2.setCell (1, 1, Math.sin (radAngle2));
 		rotationMatrix2.setCell (2, 2, Math.cos (radAngle2));
-
-		return rotationMatrix1.multiply(rotationMatrix2);
+		
+		Matrix <Double> result = new DoubleMatrix (DIM, DIM);
+		return rotationMatrix1.multiply (rotationMatrix2, result);
 
 	}
 	
@@ -123,15 +142,20 @@ public class BasicShape {
 	 * @param rotMatrix created from rotationMatrix()
 	 * @return matrix after rotation
 	 */
-	public MatrixHandler rotate(MatrixHandler rotMatrix){
+	public void rotate(Matrix<Double> rotMatrix)
+	{
 
-		for(int cCounter=0; cCounter<vectors.size();cCounter++){
-			vectors.set(cCounter,rotMatrix.multiply(vectors.get(cCounter))));
+		for(int cCounter=0; cCounter<vectors.size();cCounter++)
+		{
+			DoubleMatrix result = new DoubleMatrix (DIM, DIM);
+			DoubleMatrix vec = vectors.get (cCounter).toDoubleMatrix();
+			rotMatrix.multiply (vec, result);
+			vectors.set (cCounter, result.toIntegerMatrix());
 		}
-
+		
 	}
 	
-	private ArrayList<Matrix> vectors;
+	private ArrayList<IntegerMatrix> vectors;
 	private ArrayList<Integer> dimensions;
-	private MatrixHandler<Integer> adjMatrix;
+	private IntegerMatrix adjMatrix;
 }
