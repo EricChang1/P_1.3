93d73d60bee8c175af5e5a44154788188c7fc675
MaGa <guser1996.gu@gmail.com>
2016-01-12 14:24:06 +0100

    added matrix constructors to Glue and Position

--- algorithm/Glue.java
+++ algorithm/Glue.java
@@ -2,6 +2,8 @@ package algorithm;
 
 import java.util.ArrayList;
 
+import algorithm.Matrix.*;
+
 public class Glue implements Cloneable 
 {
 
@@ -19,9 +21,27 @@ public class Glue implements Cloneable
 			}
 	}
 	
+	@SuppressWarnings("serial")
+	public static class NotAVectorException extends IllegalArgumentException
+	{
+		public NotAVectorException() {}
+		
+		public NotAVectorException (String message) {super (message); }
+	}
+	
 	public Glue(ArrayList<Integer> position){
 		ArrayList<Integer> pos = position; 
 		if (pos.size() != 3) throw new GlueException("This ArrayList does not contain the appropriate number of positions");
+	}
+	
+	
+	public Glue (IntegerMatrix vec)
+	{
+		if (vec.getColumns() != 1)
+			throw new NotAVectorException ("Matrix of size " + vec.getRows() + " x " + vec.getColumns() + " is not a vector!");
+		ArrayList <Integer> coords = new ArrayList<Integer>();
+		for (int cRow = 0; cRow < vec.getRows(); ++cRow)
+			coords.add(vec.getCell (cRow, 0));
 	}
 	
 	public Glue clone()
