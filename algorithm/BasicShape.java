9e075824022b10ea67f7795ca2ff2a28a84b3a3b
MaGa <guser1996.gu@gmail.com>
2016-01-12 12:39:36 +0100

    added basic shape update, credits: Pablo

--- algorithm/BasicShape.java
+++ algorithm/BasicShape.java
@@ -1,5 +1,3 @@
-package algorithm;
-
 import java.util.ArrayList;
 
 public class BasicShape 
@@ -10,10 +8,18 @@ public class BasicShape
 		
 		public BadNumberOfRowsException (String message) { super (message); }
 	}
-	
+	public static class BadNumberOfCollumsException extends IllegalArgumentException
+	{
+		public BadNumberOfCollumsException() {super(); }
+		
+		public BadNumberOfCollumsException (String message) { super (message); }
+	}
 	
 	public BasicShape(ArrayList <Matrix.IntegerMatrix> vectors, Matrix.IntegerMatrix adjMatrix){
 
+		this.vectors = (ArrayList<Matrix.IntegerMatrix>) vectors.clone();
+		if (!numberOfCols(vectors)) 
+			throw new BadNumberOfCollumsException ("The vectors introduced are not 3x1");
 		dimensions = new ArrayList<Integer>();
 		calcDim (vectors);
 		this.adjMatrix = adjMatrix.clone();
@@ -26,11 +32,10 @@ public class BasicShape
 	
 	/** @return the dimensions of a shape given an index.
 	 */
-	public int getDimensions(int index){
+	public double getDimensions(int index){
 
 		return dimensions.get(index);
 	}
-	
 	/** Calculates the dimensions of a shape
 	** @param vectors ArrayList containing all the vectors
 	*/
@@ -40,7 +45,6 @@ public class BasicShape
 			throw new BadNumberOfRowsException ("vectors don't have the same dimension");
 
 		for(int i=0; i<vectors.size(); i++){
-
 
 			int max = maximum (vectors,i);
 			int min = minimum (vectors,i);
@@ -52,12 +56,21 @@ public class BasicShape
 	* @param vectors ArrayList containing all the vectors
 	* @return false if one Matrix Handler doesn't have the same number of rows
 	*/
-
 	public boolean numberOfMH(ArrayList<Matrix.IntegerMatrix> vectors){
 
 		int numberOfRows=vectors.get(0).getRows();
 		for(Matrix<Integer> temp: vectors){
 			if(temp.getRows() != numberOfRows)
+				return false;
+		}
+		return true;
+	}
+	
+	public boolean numberOfCols(ArrayList<Matrix.IntegerMatrix> vectors){
+
+		int numberOfCols=1;
+		for(Matrix<Integer> temp: vectors){
+			if(temp.getColumns()!=numberOfCols)
 				return false;
 		}
 		return true;
@@ -106,28 +119,27 @@ public class BasicShape
 	}
 	
 	/** Creates a rotation matrix based on given angles of rotation
-	 * @param angle1 Desired amount of rotation in horizontal axis (in degrees)
-	 * @param angle2 Desired amount of rotation in vertical axis (in degrees)
+	 * @param angle1 Desired amount of rotation in x2 axis (in degrees)
+	 * @param angle2 Desired amount of rotation in x3 axis (in degrees)
 	 * @return rotation matrix
 	 */
-	public Matrix rotationMatrix(double angle1, double angle2){
+	public static Matrix<Double> rotationMatrix(double angle1, double angle2){
 		double radAngle1 = Math.toRadians (angle1);
 		double radAngle2 = Math.toRadians (angle2);
-
-
-		Matrix <Double> rotationMatrix1 = new Matrix.DoubleMatrix (3, 3);
+		//rotation matrix for y axis
+		Matrix.DoubleMatrix rotationMatrix1 = new Matrix.DoubleMatrix (3, 3);
 		rotationMatrix1.setCell (0, 0, Math.cos (radAngle1));
-		rotationMatrix1.setCell (1, 0, Math.sin (radAngle1));
-		rotationMatrix1.setCell (0, 1, -Math.sin (radAngle1));
-		rotationMatrix1.setCell (1, 1, -Math.cos (radAngle1));
-		rotationMatrix1.setCell (2, 2, 1.0);
-
-		Matrix <Double> rotationMatrix2 = new Matrix.DoubleMatrix (3, 3);
+		rotationMatrix1.setCell (2, 0, -Math.sin (radAngle1));
+		rotationMatrix1.setCell (1, 1, 1.0);
+		rotationMatrix1.setCell (0, 2, Math.sin (radAngle1));
+		rotationMatrix1.setCell (2, 2, Math.cos (radAngle1));
+		//rotation matrix for z axis
+		Matrix.DoubleMatrix rotationMatrix2 = new Matrix.DoubleMatrix (3, 3);
 		rotationMatrix2.setCell (0, 0, Math.cos (radAngle2));
-		rotationMatrix2.setCell (1, 0, -Math.sin (radAngle2));
-		rotationMatrix2.setCell (0, 1, 1.0);
-		rotationMatrix2.setCell (1, 1, Math.sin (radAngle2));
-		rotationMatrix2.setCell (2, 2, Math.cos (radAngle2));
+		rotationMatrix2.setCell (1, 0, Math.sin (radAngle2));
+		rotationMatrix2.setCell (0, 1, -Math.sin (radAngle2));
+		rotationMatrix2.setCell (1, 1, Math.cos (radAngle2));
+		rotationMatrix2.setCell (2, 2, 1.0);
 
 		return rotationMatrix1.multiply (rotationMatrix2, new Matrix.DoubleMatrix (3, 3));
 	}
@@ -140,11 +152,22 @@ public class BasicShape
 
 		for(int cCounter=0; cCounter<vectors.size();cCounter++){
 			Matrix.DoubleMatrix result = new Matrix.DoubleMatrix (3,1);
-			Matrix <Double> vec = vectors.get(cCounter).toDoubleMatrix();
+			Matrix.DoubleMatrix vec = vectors.get(cCounter).toDoubleMatrix();
 			rotMatrix.multiply (vec, result);
 			vectors.set (cCounter, result.toIntegerMatrix());
 		}
-		
+
+	}
+	
+	
+	public void print()
+	{
+		System.out.println ("Printing vertices of basic shape");
+		for (Matrix.IntegerMatrix vec : vectors)
+		{
+			System.out.println ("vector");
+			vec.print(System.out);
+		}
 	}
 	
 	private ArrayList<Matrix.IntegerMatrix> vectors;
