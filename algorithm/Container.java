e7b6244f49cce832a816db59b9fb39fe29908b19
MaGa <guser1996.gu@gmail.com>
2016-01-13 12:27:13 +0100

    merged Felix' partially completed container class

--- algorithm/Container.java
+++ algorithm/Container.java
@@ -37,20 +37,24 @@ public class Container extends Block
 			super(message + " " + posFail.toString());
 		}
 	}
-
-	
 	
 	/**
-	 * @param dimensions Length of the container in the three directions
+	 * @param dimensions depth, width, height (x1,x2,x3) of the container
 	 * Calls the Block constructor with the default_value of 0
 	 */
 	public Container (ArrayList <Integer> dimensions)
 	{
-		//no look up documenation
-		super(dimensions, DEFAULT_VALUE);
+		super(dimensions, );
 	}
 	
-	
+	/**
+	 * @param dimensions depth, width, height (x1,x2,x3) of the container
+	 * @return ArrayList<Position> Containing the eight vectors that span the container
+	 */
+	public static ArrayList<Position> createVectors(ArrayList<Integer> dimensions) {
+		Position p8 = new Position(dimensions);
+		Position p1 = new Position();
+	}
 	
 	/**	Places a block at the specified position
 		@param block the block object to place
@@ -64,11 +68,8 @@ public class Container extends Block
 	{
 		if (checkPositionOverlap(block, pos)) {
 				Glue glue = pos.clone();
-				//Nothing implemented in glue that I could use to place the block
-				//placing is done by the container => store in hashmap
-				block.glueBlock(pos);
-				//@TODO edit make sure adjacency matrix is edited and list of vectors in basic shape
-				//this container inherits from
+				mGluedBlocks.put(glue, block);
+				addShape(block);
 		}
 		else throw new WrongPositionException("position is not inside of the container", pos);
 	}
@@ -82,48 +83,15 @@ public class Container extends Block
 		if (Block.getValue(pos) != 0) {
 			return Block.getValue(pos);
 		}
-		else throw BockNotFoundException(pos);
+		else throw new BlockNotFoundException(pos);
 	}
 	
 	/** @return a string representation of the container**/
 	public String toString()
 	{
-		return "Height: " + this.getHeight() + ", width: " + this.getWidth() + " glues at: " + this.mGluedBlocks;
+		return "Height: " + this.getHeight() + ",\n width: " + this.getWidth() + "\nglues at: " + this.mGluedBlocks;
 	}
 	
-	/**	@param block the block object to place
-		@param pos the position to place block 
-		@return true if block placed at pos remains within the container
-		The position refers to the top-left corner of the smallest possible rectangle containig the block
-
-		 **edit: not really needed anymore :(
-		 *
-	public boolean checkPositionValid (Block block, Position pos)
-	{
-		ArrayList<Integer> position = pos.getPosition();
-			
-		//First check: Is pos (equivalent to minPos) inside the borders of this container?
-		for (int i=0; i<position.length; i++) {
-			int minDim = position.get(i);
-			if ((minDim < 0) || (minDim > this.getDimension(i))) return false;
-		}
-		
-		Position maxDim = block.getMaxDimension(pos);
-		ArrayList<Integer> maxPos = maxDim.getPosition();
-		
-		//Second check: Is the maxPos also inside of this container`?
-		for (int i=0; i<maxPos.length; i++) {
-			int maxDim = maxPos.get(i);
-			if ((maxDim < 0)||(maxDim > this.getDimension(i))) return false;
-		}
-				
-		return true;
-	}
-
-		**/
-
-
-
 	/**	@param block the block object to place
 		@param pos the position to place block 
 		@return true if block placed at pos does not cause any overlapping with previously placed blocks
@@ -131,36 +99,38 @@ public class Container extends Block
 	**/
 	public boolean checkPositionOverlap (Block block, Position pos)
 	{
-		ArrayList<Integer> minPos = pos.getPosition();
-		Position maxDim = block.getMaxDimension(pos);
-		ArrayList<Integer> maxPos = maxDim.getPosition();
-
-		ArrayList<Double> l1 = new Line(minPos, maxPos);
-		//doesn't work
-		//check: for every connection c1 between two vertices of block
-			//and for every connection c2 within the container (including pieces already placed)
-				//whether c1 and c2 intersect
-		for (int i=0; i<mGluedBlocks.size(); i++){
-			if (Line.doIntersect(l1, mGluedBlocks.get(i))) return false;
+		
+		ArrayList<Line> blockVertices = new ArrayList<Line>();
+		
+		for (int i=0; i < block.getNumberOfVertices(); i++) {
+			Matrix<Integer> p = block.getVertex(i);
+			ArrayList<Matrix.IntegerMatrix> connections = p.lookUpConnections(i);
+			for (int j=0; j<connections.size(); j++) {
+				//duplicates alarm!
+				Line l1 = new Line(p, connections.get(j));
+				blockVertices.add(l1);
+			}
+		}
+		
+		int index = getNumberOfVertices();
+		
+		for (int i=0; i < index; i++) {
+			ArrayList<Matrix.IntegerMatrix> connections = lookUpConnections(i);
+			Matrix<Integer> point = getVertex(i);
+			
+			for (int j=0; j < connections.size(); j++) {
+				Line l2 = new Line(point, connections.getCell(j));
+				if (blockVertices.get(j).doIntersect(l2)) return false;
+			}
 		}
 		return true;
 	}
-	
-	//unnecessary
-	/**	@param block the block object to place
-		@param pos the position to place block 
-		@return true if block placed at pos remains within the container and does not cause any overlapping with previously placed blocks
-		The position refers to the top-left corner of the smallest possible rectangle containig the block
-	**/
-	public boolean checkPosition (Block block, Position pos)
-	{
-		//if (checkPositionValid(block, pos)) {
-			if (checkPositionOverlap(block, pos)) return true;				
-		//}
-		return false;
-	}
 
+	public int getAmountOfBlocks() {
+		return mGluedBlocks.size();
+	}
+	
 	private HashMap <Glue, Block> mGluedBlocks;
 	
-	private final static int DEFAULT_VALUE;
+	
 }
