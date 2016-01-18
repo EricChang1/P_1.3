983d085307af644e375fe02ddbbf4a543f98d4c3
EricChang1 <ericchangeu@gmail.com>
2016-01-18 18:39:12 +0100

    Fixed errors in scaler class

--- algorithm/Scaler.java
+++ algorithm/Scaler.java
@@ -24,24 +24,26 @@ public class Scaler
 		precondition: vectors needs to contain only matrices with one column and the same amount of rows
 		@throws InconsistentVectorsException
 	**/
-	public Scaler (ArrayList <MatrixHandler> vec)
+	public Scaler (ArrayList <Matrix> vec)
 	{
 		//Clone vec so no changes are applied to original
-		ArrayList <MatrixHandler> vectors = vec.clone();
+		ArrayList <Matrix> vectors = (ArrayList<Matrix>) vec.clone();
 		
 		//Method should exist in MatrixHandler
-		if (dimensionEquals(false))
-		{
-			InconsistentVectorsException exception = new InconsistentVectorsException
-			("Vectors are not appropiate size"); 
-		}
+		for (int i=0; i<vectors.size(); i++)
+			if (vectors.get(i).dimensionEquals(vectors.get(0))==false)
+			{
+				InconsistentVectorsException exception = new InconsistentVectorsException
+				("Vectors are not appropiate size"); 
+			}
 		
 		//Loops through all vectors and find the smallest
 		for (int i=0; i<vectors.size();i++)
 		{
-			for (int j=0; j<vectors.getRows();j++)
+			for (int j=0; j<vectors.get(i).getRows();j++)
 			{
-				double currentValue = vectors.get(i).getRow(j).getCell(); //value in current cell
+				Number numValue = vectors.get(i).getCell(j, 1); //value in current cell
+				double currentValue = numValue.doubleValue();
 				int digits = String.valueOf(currentValue).length(); //number of digits in current cell
 				int intLength = (int) (Math.floor(currentValue)); //number of digits in cell ignoring decimals
 				if (smallestValue == 0.0 ||smallestValue>currentValue)
@@ -65,14 +67,14 @@ public class Scaler
 		
 		
 	}
-	public double commFacFinder(ArrayList <MatrixHandler> vectors)
+	public double commFacFinder(ArrayList <Matrix> vectors)
 	{
 		//Temporary number multiplied to vector to remove all decimals when searching for common factor
 		double removeDecimal = Math.pow(10, numDecimal);
 		//Maximum potential common factor
 		double half = Math.floor(0.5*smallestValue*removeDecimal);
 		
-		ArrayList <MatrixHandler> scaledVectors = scale(vectors, removeDecimal);
+		ArrayList <Matrix> scaledVectors = scale(vectors, removeDecimal);
 		
 		//Return the lowest common factor, 1 is not needed because it has to be common factor
 		for (double i=2; i<=half;i++)
@@ -89,14 +91,16 @@ public class Scaler
 	}
 	
 	
-	public boolean commFacCheck(ArrayList <MatrixHandler> vectors, double commFac)
+	public boolean commFacCheck(ArrayList <Matrix> vectors, double commFac)
 	{
 		boolean check=true;
 			for (int i=0; i<vectors.size();i++)
 			{
-				for (int j=0; j<vectors.getRows();j++)
+				for (int j=0; j<vectors.get(i).getRows();j++)
 				{
-					if (vectors.get(i).getRow(j).getCell()%commFac!=0)
+					Number numValue = vectors.get(i).getCell(j, 1); //value in current cell
+					double currentValue = numValue.doubleValue();
+					if (currentValue%commFac!=0)
 					{
 						check=false;
 					}
@@ -110,21 +114,26 @@ public class Scaler
 		precondition: vectors needs to contain only matrices with one column and the same amount of rows
 		@throws InconsistentVectorsException
 	**/
-	public ArrayList <MatrixHandler> scale (ArrayList <MatrixHandler> vectors, double scaler)
+	public ArrayList <Matrix> scale (ArrayList <Matrix> vectors, double scaler)
 	{
-		if (dimensionEquals(false))
-		{
-			InconsistentVectorsException exception = new InconsistentVectorsException("Vectors are not appropiate size"); 
-		}
+		for (int i=0; i<vectors.size(); i++)
+			if (vectors.get(i).dimensionEquals(vectors.get(0))==false)
+			{
+				InconsistentVectorsException exception = new InconsistentVectorsException
+				("Vectors are not appropiate size"); 
+			}
 		for (int i=0; i<vectors.size();i++)
 		{
-			for (int j=0; j<vectors.getRows();j++)
+			for (int j=0; j<vectors.get(i).getRows();j++)
 			{
-				double currentCell = vectors.get(i).getRow(j);
-				double currentValue = vectors.get(i).getRow(j).getCell();
-				currentCell.setCell(currentValue*scaler);
+				Number numValue = vectors.get(i).getCell(j, 1); //value in current cell
+				double currentValue = numValue.doubleValue();
+				vectors.get(i).setCell(1, 1, 1);
+				Number newValue = currentValue*scaler;
+				vectors.get(i).setCell(i, 1, newValue);
 			}
 		}
+		return vectors;
 	}
 
 	/** @return scaling factor**/
@@ -138,28 +147,6 @@ public class Scaler
 	 * @return true vectors are different sizes
 	 */
 	
-	/*		//Method should no longer be needed
-	public boolean vectorSizeFalseCheck(ArrayList<MatrixHandler> vectors)
-	{
-		for (int i=0; i<vectors.size();i++)
-		{
-			if (vectors.get(i).getColumns=1)
-			{
-				//save current vector height
-				int vecHeight = vectors.get(i);
-				//check if current vector has same height as previous
-				if (i=0|| vecHeight!=vectors.get(i-1).getRows)
-				{
-					// if they don't have same height, return true;
-					return true;
-				}
-			}
-		}
-		//if for-loop completes, return false
-		return false;
-	}
-	*/
-
 	private double mScalingFactor; //Number that will be applied to all vectors in the END to make them all integers
 	private double smallestValue; //Stores the smallest value in an arraylist of integers
 	private int numDecimal; // Stores the number of decimals in a value. NOTE: 1.0 would results in numDecimal = 1
